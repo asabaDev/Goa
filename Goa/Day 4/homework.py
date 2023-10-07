d96fa34fcaa537356c29e494dc13eba09e70f82d
asabaDev <146023437+asabaDev@users.noreply.github.com>
2023-10-07 12:22:49 +0400

    Add files via upload

---
+++ Goa/Day 4/homework.py
@@ -0,0 +1,29 @@
+# Your family members and their ages
+father = "Giorgi"
+father_age = 38
+
+mother = "Natia"
+mother_age = 36
+
+child = "saba"
+child_age = 15
+
+# Variables after 20 years
+father_after_20_years = "Giorge"
+father_age_after_20_years = 58
+
+mother_after_20_years = "Natia"
+mother_age_after_20_years = 56
+
+child_after_20_years = "saba"
+child_age_after_20_years = 35
+
+# Printing
+print("Name of father after 20 years:", father_after_20_years)
+print("Age of father after 20 years:", father_age_after_20_years)
+
+print("Name of mother after 20 years:", mother_after_20_years)
+print("Age of mother after 20 years:", mother_age_after_20_years)
+
+print("Name of child after 20 years:", child_after_20_years)
+print("Age of child after 20 years:", child_age_after_20_years)
