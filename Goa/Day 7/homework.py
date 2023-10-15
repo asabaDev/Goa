d7c8c5ceb1ce8eff442dcda2829e7f780a0903f8
asabaDev <146023437+asabaDev@users.noreply.github.com>
2023-10-15 20:01:19 +0400

    Add files via upload

---
+++ Goa/Day 7/homework.py
@@ -0,0 +1,9 @@
+# defining the ticket price and the number of people
+ticket_price = 25
+adults = 10
+children = 3
+
+# calculating the total cost
+total_cost = adults * ticket_price # as tickets for children are free
+
+print(f"The total amount needed is {250} lari.")
